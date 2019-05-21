65f8196b22d8f05433089d16cdc6818764e61e55
Mike Owens <mocoding86@gmail.com>
2019-05-20 20:37:21 -0700

    adjusted folders

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 let randomNumberOne = (Math.floor(Math.random() * 6) + 1);
 let randomNumberTwo = (Math.floor(Math.random() * 6) + 1);
 
-document.querySelector(".img1").setAttribute("src", `images/dice${randomNumberOne}.png`);
-document.querySelector(".img2").setAttribute("src", `images/dice${randomNumberTwo}.png`);
+document.querySelector(".img1").setAttribute("src", `img/dice${randomNumberOne}.png`);
+document.querySelector(".img2").setAttribute("src", `img/dice${randomNumberTwo}.png`);
 
 randomNumberOne === randomNumberTwo ? document.querySelector("h1").textContent = "Draw!"
 : randomNumberOne < randomNumberTwo ? document.querySelector("h1").textContent = "Player 2 WINS!!🏁"
